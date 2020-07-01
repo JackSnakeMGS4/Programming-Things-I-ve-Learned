ce58a553cd3e9012178b92f9e96a6621de473074
Jaime Rivas <jaimerivas81@outlook.com>
2020-06-30 17:37:50 -0700

    WIP get(index) function; shift and unshift methods

--- Doubly Linked List/doubleLinkedList.js
+++ Doubly Linked List/doubleLinkedList.js
@@ -49,11 +49,72 @@ class DoublyLinkedList{
 		return oldTail;
 	}
 
-	shift(value){
-		// add node to beginning of list
+	shift(){
+		// remove node from beginning of list
+		if(!this.head) return undefined;
+
+		let oldHead = this.head;
+
+		if(this.length === 1){
+			this.head = null;
+			this.tail = null;
+		}
+		else{
+			this.head = oldHead.next;
+			this.head.prev = null;
+		}
+		oldHead.next = null;
+
+		this.length--;
+		return oldHead;
 	}
 
-	unshift(){
-		// remove node from beginning of list
+	unshift(value){
+		// add node to beginning of list
+		let node = new Node(value);
+
+		if(!this.head){
+			this.head = node;
+			this.tail = node;
+		}
+		else{
+			this.head.prev = node;
+			node.next = this.head;
+			this.head = node;
+		}
+
+		this.length++;
+
+		return this;
+	}
+
+	get(index){
+		// depending on the index either search from the
+		// list's head or tail
+		// I.E. list length = 10 so index 3 (fourth item) is closer to head
+		// so start search from head
+
+		if(index < 0 || index >= this.length) return null;
+		switch(index){
+			case index < 0:
+			case index >= this.length:
+				return null;
+				break;
+			case index <= (this.length/2-1):
+				return this._searchHelper(true);
+				break;
+			case index > (this.length/2-1):
+				return this._searchHelper(false);
+				break;
+		}
+	}
+
+	set(index, value){
+
+	}
+
+	// Helper function for looping through lists depending on whether index is > or <= list.length - 1;
+	_searchHelper(shouldLoopFromHead){
+		
 	}
 }
